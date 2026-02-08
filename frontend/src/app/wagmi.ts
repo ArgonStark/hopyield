53761541c11e71e80ef08a372db0a37cbc894151
Argon <argon@Argons-MacBook-Pro.local>
2026-02-08 12:07:23 +0330

    update networks

--- frontend/src/app/wagmi.ts
+++ frontend/src/app/wagmi.ts
@@ -1,9 +1,9 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { base, arbitrum } from "wagmi/chains";
+import { baseSepolia, arbitrumSepolia } from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "UniYield",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
-  chains: [base, arbitrum],
+  chains: [baseSepolia, arbitrumSepolia],
   ssr: true,
 });
